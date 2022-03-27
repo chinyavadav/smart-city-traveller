d2535cb64be7febc5f93a1befb0225dc1d0cfd9e
Victor Chinyavada <vchinyavada@zss.co.zw>
2022-03-27 12:34:01 +0200

    change

--- app/src/main/java/com/smartcitytraveller/mobile/common/Constants.java
+++ app/src/main/java/com/smartcitytraveller/mobile/common/Constants.java
@@ -1,7 +1,6 @@
 package com.smartcitytraveller.mobile.common;
 
 public class Constants {
-    public static final String CORE_BASE_URL = "http://connect-us-service.herokuapp.com";
+    public static final String CORE_BASE_URL = "http://smart-city-traveller.herokuapp.com";
     public static final String GENERAL_TOPIC = "general";
-    public static final String AGENT_TOPIC = "paymate";
 }
