2274f13370e67632a852df72ed4e560242ccc023
Victor Chinyavada <chinyavadav@gmail.com>
2022-05-05 03:35:09 +0200

    add google-java-format.xml - update url

--- app/src/main/java/com/smartcitytraveller/mobile/common/Constants.java
+++ app/src/main/java/com/smartcitytraveller/mobile/common/Constants.java
@@ -1,6 +1,5 @@
 package com.smartcitytraveller.mobile.common;
 
 public class Constants {
-    public static final String CORE_BASE_URL = "http://smart-city-traveller.herokuapp.com";
-    public static final String GENERAL_TOPIC = "general";
+    public static final String CORE_BASE_URL = "http://54.158.205.197:8195";
 }
